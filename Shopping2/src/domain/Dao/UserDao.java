78ce9ad7f44bf7c1962bdae9a9af023aed7c05ee
piepenie <142890839+piepenie@users.noreply.github.com>
2024-04-02 02:32:07 +0900

    Update UserDao.java

--- Shopping2/src/domain/Dao/UserDao.java
+++ Shopping2/src/domain/Dao/UserDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import domain.Dto.UserDto;
 
@@ -18,26 +19,49 @@ public class UserDao {
 	
 	
 	public UserDao() throws Exception {
-		Class.forName("");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		conn = DriverManager.getConnection(url, id, pw);
 		System.out.println("[DAO] UserDao's INIT DB Connected...");
 	}
 	
 	//INSERT 
-	public boolean Insert(UserDto dto) throws Exception{
-		pstmt = conn.prepareStatement("insert into user values(?,?,?,?,?,?)");
-		pstmt.setString(1, dto.getUser_id());
-		pstmt.setString(2, dto.getPassword());
-		pstmt.setString(3, dto.getName());
-		pstmt.setString(4, dto.getPhone_number());
-		pstmt.setString(5, dto.getEmail());
-		pstmt.setString(6, dto.getRole());
+	public boolean Insert(UserDto userDto) throws SQLException{
+		pstmt = conn.prepareStatement("insert into user values(?,?,?,?,?,?,?)");
+		pstmt.setInt(1, userDto.getId());
+		pstmt.setString(2, userDto.getUser_id());
+		pstmt.setString(3, userDto.getPassword());
+		pstmt.setString(4, userDto.getName());
+		pstmt.setString(5, userDto.getPhone_number());
+		pstmt.setString(6, userDto.getEmail());
+		pstmt.setString(7, userDto.getRole());
 		
 		int result = pstmt.executeUpdate();
 		
 		pstmt.close();
+		conn.close();
 		
-		return result >0;
+		return result>0;
+	}
+	
+	//UPDATE
+	public boolean Update(UserDto userDto) throws SQLException{
+		pstmt = conn.prepareStatement("update user set ");
+		pstmt.setInt(1, userDto.getId());
+		pstmt.setString(2, userDto.getUser_id());
+		pstmt.setString(3, userDto.getPassword());
+		pstmt.setString(4, userDto.getName());
+		pstmt.setString(5, userDto.getPhone_number());
+		pstmt.setString(6, userDto.getEmail());
+		pstmt.setString(7, userDto.getRole());
+		
+		int result = pstmt.executeUpdate();
+		
+		
+		pstmt.close();
+		conn.close();
+		
+		return result>0;
+		
 	}
 	
 	
