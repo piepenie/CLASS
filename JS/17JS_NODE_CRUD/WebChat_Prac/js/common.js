ee0bd1f13dd70f05587501f4f68dd1834e2c5e4c
piepenie <142890839+piepenie@users.noreply.github.com>
2024-04-12 13:35:52 +0900

    Add files via upload

---
+++ JS/17JS_NODE_CRUD/WebChat_Prac/js/common.js
@@ -0,0 +1,35 @@
+// EVENT
+const mainSectionEl = document.querySelector('main>section');
+mainSectionEl.addEventListener('dragover',function(event){
+
+})
+mainSectionEl.addEventListener('dragleave',function(event){
+
+})
+ 
+mainSectionEl.addEventListener('drop',function(event){
+
+    
+})
+
+const textAreaEl =  document.querySelector('.wrapper>footer .footer-input textarea');
+textAreaEl.addEventListener('keydown',function(e){
+
+});
+
+const sendBtn =  document.querySelector('.wrapper>footer .footer-bottom button.send');
+sendBtn.addEventListener('click',function(e){
+
+    console.log(e);
+});
+
+
+function createChatNode_You(data){
+
+
+}
+
+function createChatNode_Me(data,file){
+
+
+}
